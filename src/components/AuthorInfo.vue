a7ebb9276515de66eae887f6e972e7aac6601b5a
Stepan Shtennikov <shtennikov.s@gmail.com>
2024-01-03 16:31:05 +0500

    feat: implement author info component

---
+++ src/components/AuthorInfo.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li>{{ author.name }}</li>
+        <li>{{ author.email }}</li>
+        <li>{{ author.phone }}</li>
+        <li>{{ author.website }}</li>
+    </ul>
+</template>
+
+<script setup lang="ts">
+import type { User } from '../types/User.interface';
+
+defineProps<{ author: User }>();
+</script>
+
+<style scoped></style>
