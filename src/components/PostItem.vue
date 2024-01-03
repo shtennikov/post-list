73c43ec4a2064f8d5a5f3d67a7128b98a95ec85c
Stepan Shtennikov <shtennikov.s@gmail.com>
2024-01-03 13:14:44 +0500

    feat: implement a post item component

---
+++ src/components/PostItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="post-item">
+        <h3>{{ post.title }}</h3>
+        <article>{{ post.body }}</article>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Post } from '../types/Post.interface';
+
+defineProps<{ post: Post }>();
+</script>
+
+<style scoped lang="scss">
+.post-item {
+    max-width: 500px;
+}
+</style>
