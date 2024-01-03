d5e66488e9f6d80a272d25a8a6951111c52ae50f
Stepan Shtennikov <shtennikov.s@gmail.com>
2024-01-03 13:09:40 +0500

    feat: implement a post list component

---
+++ src/components/PostList.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul class="post-list">
+        <li v-for="post in posts" :key="post.id">
+            <RouterLink :to="{ name: 'post', params: { postId: post.id } }">
+                <PostItem :post="post" />
+            </RouterLink>
+        </li>
+    </ul>
+</template>
+
+<script setup lang="ts">
+import type { Post } from '../types/Post.interface';
+import PostItem from './PostItem.vue';
+
+defineProps<{ posts: Post[] }>();
+</script>
+
+<style scoped>
+.post-list {
+    margin: auto;
+    max-width: fit-content;
+}
+</style>
