6289da8d0b1e394dcff3a4bf291263f6522d280c
Stepan Shtennikov <shtennikov.s@gmail.com>
2024-01-04 10:06:53 +0500

    feat: style the post item

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,7 +1,7 @@
 <template>
     <article class="post-item">
-        <h3>{{ post.title }}</h3>
-        <p>{{ post.body }}</p>
+        <h3 class="post-item__title">{{ post.title }}</h3>
+        <p class="post-item__body">{{ post.body }}</p>
     </article>
 </template>
 
@@ -13,6 +13,28 @@ defineProps<{ post: Post }>();
 
 <style scoped lang="scss">
 .post-item {
-    max-width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 20px;
+
+    padding: 20px 30px;
+    max-width: 700px;
+    border: 2px solid $border-color;
+    border-radius: 40px;
+    transition: transform 0.3s ease;
+
+    &__title {
+        color: $text-title-color;
+    }
+
+    &__body {
+        color: $text-primary-color;
+    }
+
+    a &:hover {
+        border-color: $accent-color;
+        transform: scale(1.02);
+    }
 }
 </style>
